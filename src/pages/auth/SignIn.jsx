b6ae33974862e23e11ee001a8833f8253a2fe66a
michalkarbasz <michal.karbasz@gmail.com>
2020-09-18 20:15:18 +0200

    connect sign in to firebase

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -5,14 +5,21 @@ import './sign-in.scss';
 import FormInput from '../../components/form-input/FormInput';
 import CustomButton from '../../components/custom-button/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   state = { email: '', password: '' };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = event => {
