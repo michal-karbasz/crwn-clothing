aa5cc7c245336255fe5b384d4c5f651be59d32c2
michalkarbasz <michal.karbasz@gmail.com>
2020-09-19 20:05:47 +0200

    removed redundant code, added redirect for signed users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -16,20 +16,20 @@ class App extends Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
           setCurrentUser({
-            currentUser: { id: snapshot.id, ...snapshot.data() },
+            id: snapshot.id,
+            ...snapshot.data(),
           });
         });
       }
 
-      setCurrentUser({ userAuth });
-
-      createUserProfileDocument(userAuth);
+      setCurrentUser(userAuth);
     });
   }
 
@@ -44,15 +44,25 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/auth" component={AuthWrapper} />
+          <Route
+            exact
+            path="/auth"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <AuthWrapper />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
