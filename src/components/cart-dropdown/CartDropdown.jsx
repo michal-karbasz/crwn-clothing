6df4b833c799237ee2bda765347b41c17d36ba19
michalkarbasz <michal.karbasz@gmail.com>
2020-10-10 00:43:51 +0200

    add checkout item components and styles

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -8,8 +8,9 @@ import './cart-dropdown.scss';
 import { selectCartItems } from '../../redux/cart/cartSelectors';
 import CustomButton from '../custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import { toggleCartHidden } from '../../redux/cart/cartActions';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -19,7 +20,12 @@ const CartDropdown = ({ cartItems, history }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push('/checkout')}>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
         go to checkout
       </CustomButton>
     </div>
--- src/pages/checkout/CheckoutWrapper.jsx
+++ src/pages/checkout/CheckoutWrapper.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import CheckoutItem from '../../components/checkout-item/CheckoutItem';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from '../../redux/cart/cartSelectors';
 
 import './checkout.scss';
 
-const CheckoutWrapper = () => {
-  return <div>Checkout</div>;
+const CheckoutWrapper = ({ cartItems, total }) => {
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        <span>Total: ${total}</span>
+      </div>
+    </div>
+  );
 };
 
-export default CheckoutWrapper;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutWrapper);
