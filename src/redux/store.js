8208d37ff55be67febbc33b0a99633ea5ba4d7ff
michalkarbasz <michal.karbasz@gmail.com>
2021-01-15 17:29:46 +0100

    make redux logger env dependant

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,11 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
 
 const middlewares = [logger];
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const isDevelopment = process.env.NODE_ENV === 'development';
+const store = createStore(
+  rootReducer,
+  isDevelopment && applyMiddleware(...middlewares)
+);
 const persistor = persistStore(store);
 
 export { store, persistor };
